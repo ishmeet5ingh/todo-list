a0addcc42a324e1a33af3aaca3a6f5495b742d1a
ishmeet5ingh <ishmeet3j@gmail.com>
2024-04-13 01:36:27 +0530

    updated

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -18,7 +18,7 @@ function TodoForm() {
       <input
         type="text"
         placeholder="Write Todo..."
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        className="w-full border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5 mr-4"
         value={todo}
         // wiring of input to state.
 
@@ -27,7 +27,7 @@ function TodoForm() {
       />
       <button
         type="submit"
-        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        className=" px-3 py-1 bg-green-600 text-white shrink-0"
       >
         Add
       </button>
